76345d9556e6f03db7dfb47a41875391f37665cf
Jason <jason@hedmans.org>
2025-07-28 15:42:20 -0400

    modularize toolkit list items

---
+++ src/components/toolkit/toolkit-list/index.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandList,
+} from "@/components/ui/command";
+
+import { ToolkitItem } from "./item";
+
+import { clientToolkits } from "@/toolkits/toolkits/client";
+
+import type { ClientToolkit } from "@/toolkits/types";
+import type { Toolkits } from "@/toolkits/toolkits/shared";
+import type { SelectedToolkit } from "../types";
+
+interface ToolkitListProps {
+  selectedToolkits: SelectedToolkit[];
+  onAddToolkit: (toolkit: SelectedToolkit) => void;
+  onRemoveToolkit: (id: Toolkits) => void;
+}
+
+const toolkitItemHeight = 48;
+const numToolkitsToShow = 5;
+
+export const ToolkitList: React.FC<ToolkitListProps> = ({
+  selectedToolkits,
+  onAddToolkit,
+  onRemoveToolkit,
+}) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const updatedToolkits = Object.entries(clientToolkits).filter(([id]) => {
+      return (
+        searchParams.get(id) === "true" &&
+        !selectedToolkits.some((t) => t.id === (id as Toolkits))
+      );
+    });
+
+    if (updatedToolkits.length > 0) {
+      updatedToolkits.forEach(([id, toolkit]) => {
+        onAddToolkit({
+          id: id as Toolkits,
+          parameters: {},
+          toolkit: toolkit as ClientToolkit,
+        });
+      });
+      window.history.replaceState({}, "", pathname);
+    }
+  }, [searchParams, onAddToolkit, selectedToolkits, router, pathname]);
+
+  return (
+    <Command className="bg-transparent">
+      <CommandInput
+        placeholder="Search toolkits..."
+        value={searchQuery}
+        onValueChange={setSearchQuery}
+      />
+      <CommandList
+        style={{
+          height: `${toolkitItemHeight * (numToolkitsToShow + 0.5)}px`,
+        }}
+      >
+        <CommandEmpty>No toolkits match your search</CommandEmpty>
+        <CommandGroup className="p-0">
+          {Object.entries(clientToolkits).map(([id, toolkit]) => {
+            const typedId = id as Toolkits;
+            return (
+              <ToolkitItem
+                key={typedId}
+                id={typedId}
+                toolkit={toolkit as ClientToolkit}
+                isSelected={selectedToolkits.some((t) => t.id === typedId)}
+                addToolkit={onAddToolkit}
+                removeToolkit={onRemoveToolkit}
+              />
+            );
+          })}
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
+};
---
+++ src/components/toolkit/toolkit-list/item.tsx
@@ -0,0 +1,164 @@
+import { useState } from "react";
+
+import { Loader2, Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { CommandItem } from "@/components/ui/command";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { VStack } from "@/components/ui/stack";
+
+import { cn } from "@/lib/utils";
+
+import type {
+  ServerToolkitParameters,
+  Toolkits,
+} from "@/toolkits/toolkits/shared";
+import type { ClientToolkit } from "@/toolkits/types";
+import type { SelectedToolkit } from "../types";
+
+interface Props {
+  id: Toolkits;
+  toolkit: ClientToolkit;
+  isSelected: boolean;
+  addToolkit: (toolkit: SelectedToolkit) => void;
+  removeToolkit: (id: Toolkits) => void;
+}
+
+export const ToolkitItem: React.FC<Props> = ({
+  id,
+  toolkit,
+  isSelected,
+  addToolkit,
+  removeToolkit,
+}) => {
+  const BaseItem = ({
+    onSelect,
+    isLoading,
+  }: {
+    onSelect: () => void;
+    isLoading: boolean;
+  }) => {
+    return (
+      <CommandItem
+        onSelect={onSelect}
+        className="flex items-center gap-2 rounded-none px-3"
+        disabled={isLoading}
+        value={toolkit.name}
+      >
+        <div
+          className={cn(
+            "rounded-full border p-1",
+            isSelected && "border-primary text-primary",
+          )}
+        >
+          {isLoading ? (
+            <Loader2 className="size-4 animate-spin" />
+          ) : (
+            <toolkit.icon
+              className={cn("size-4", isSelected && "text-primary")}
+            />
+          )}
+        </div>
+
+        <VStack className="flex flex-1 flex-col items-start gap-0">
+          <h3 className="text-sm font-medium">{toolkit.name}</h3>
+          <p className="text-muted-foreground text-xs">{toolkit.description}</p>
+        </VStack>
+      </CommandItem>
+    );
+  };
+
+  const ItemWithForm = ({
+    onSelect,
+    isLoading,
+  }: {
+    onSelect?: () => void;
+    isLoading: boolean;
+  }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [parameters, setParameters] = useState<
+      ServerToolkitParameters[typeof id]
+    >({} as ServerToolkitParameters[typeof id]);
+
+    return (
+      <>
+        <BaseItem
+          isLoading={isLoading}
+          onSelect={onSelect ?? (() => setIsOpen(true))}
+        />
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+          <DialogContent className="gap-2 p-0 sm:max-w-[425px]">
+            <DialogHeader className="p-4 pb-0">
+              <DialogTitle>Configure {toolkit.name}</DialogTitle>
+              <DialogDescription className="sr-only">
+                Configure the {toolkit.name} toolkit
+              </DialogDescription>
+            </DialogHeader>
+            {toolkit.form && (
+              <toolkit.form
+                parameters={parameters}
+                setParameters={setParameters}
+              />
+            )}
+            <DialogFooter className="flex justify-end gap-2 p-4">
+              <Button variant="outline" onClick={close} className="flex-1">
+                Cancel
+              </Button>
+              <Button
+                onClick={() =>
+                  addToolkit({
+                    id,
+                    toolkit,
+                    parameters,
+                  })
+                }
+                disabled={!toolkit.parameters.safeParse(parameters).success}
+                className="flex-1"
+              >
+                <Plus className="size-4" />
+                Enable
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </>
+    );
+  };
+
+  const Item = ({
+    isLoading,
+    onSelect,
+  }: {
+    isLoading: boolean;
+    onSelect?: () => void;
+  }) => {
+    if (Object.keys(toolkit.parameters.shape).length > 0) {
+      return <ItemWithForm isLoading={isLoading} onSelect={onSelect} />;
+    }
+
+    return (
+      <BaseItem
+        isLoading={isLoading}
+        onSelect={
+          onSelect ??
+          (isSelected
+            ? () => removeToolkit(id)
+            : () => addToolkit({ id, toolkit, parameters: {} }))
+        }
+      />
+    );
+  };
+
+  if (toolkit.Wrapper) {
+    return <toolkit.Wrapper key={id} Item={Item} />;
+  }
+
+  return <Item key={id} isLoading={false} />;
+};
