4838c5a862b2c5225c526e2f59163f6c6b48e67f
Jason <jason@hedmans.org>
2025-07-30 14:24:22 -0400

    fmt

--- src/app/_components/auth/auth-buttons.tsx
+++ src/app/_components/auth/auth-buttons.tsx
@@ -15,28 +15,30 @@ interface AuthButtonsProps {
 export const AuthButtons = ({ providers, redirect }: AuthButtonsProps) => {
   return (
     <div className="flex flex-col gap-2">
-      {providers.filter((provider) => provider.id !== "spotify").map((provider) =>
-        provider.id !== "credentials" ? (
-          <Button
-            key={provider.id}
-            variant="outline"
-            className="w-full"
-            onClick={() => signIn(provider.id, { redirectTo: redirect })}
-          >
-            <AuthProviderIcon provider={provider.name} />
-            Sign in with {provider.name}
-          </Button>
-        ) : (
-          <Button
-            key={provider.id}
-            variant="outline"
-            className="w-full"
-            onClick={() => signIn("guest", { redirectTo: redirect })}
-          >
-            Sign in as Guest
-          </Button>
-        ),
-      )}
+      {providers
+        .filter((provider) => provider.id !== "spotify")
+        .map((provider) =>
+          provider.id !== "credentials" ? (
+            <Button
+              key={provider.id}
+              variant="outline"
+              className="w-full"
+              onClick={() => signIn(provider.id, { redirectTo: redirect })}
+            >
+              <AuthProviderIcon provider={provider.name} />
+              Sign in with {provider.name}
+            </Button>
+          ) : (
+            <Button
+              key={provider.id}
+              variant="outline"
+              className="w-full"
+              onClick={() => signIn("guest", { redirectTo: redirect })}
+            >
+              Sign in as Guest
+            </Button>
+          ),
+        )}
     </div>
   );
 };
