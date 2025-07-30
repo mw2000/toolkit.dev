da19224261e83b5f005e192980bceb8f06cbb993
Jason <jason@hedmans.org>
2025-07-30 14:08:53 -0400

    add beta access flag on Spotify toolkit

--- src/toolkits/toolkits/spotify/wrapper.tsx
+++ src/toolkits/toolkits/spotify/wrapper.tsx
@@ -20,14 +20,40 @@ export const SpotifyWrapper: ClientToolkitWrapper = ({ Item }) => {
   const { data: hasAccount, isLoading } =
     api.accounts.hasProviderAccount.useQuery("spotify");
 
+  const { data: hasAccess, isLoading: isLoadingAccess } =
+    api.features.hasFeature.useQuery({
+      feature: "spotify",
+    });
+
   const [isAuthRequiredDialogOpen, setIsAuthRequiredDialogOpen] =
     useState(false);
+  const [isPrivateBetaDialogOpen, setIsPrivateBetaDialogOpen] =
+    useState(false);
 
-  if (isLoading) {
+  if (isLoading || isLoadingAccess) {
     return <Item isLoading={true} />;
   }
 
-  if (!hasAccount) {
+  if (!hasAccess) {
+    return (
+      <>
+        <Item
+          isLoading={isLoading || isLoadingAccess}
+          onSelect={() => setIsPrivateBetaDialogOpen(true)}
+        />
+        <AuthRequiredDialog
+          isOpen={isPrivateBetaDialogOpen}
+          onOpenChange={setIsPrivateBetaDialogOpen}
+          Icon={SiSpotify}
+          title="Beta Access Required"
+          description="We need to add you as a test user on Spotify for us to request sensitive OAuth scopes. Please contact @jsonhedman on X to request access. You can also run the project locally and use your own Spotify account."
+          content={null}
+        />
+      </>
+    );
+  }
+
+  if (!hasAccount || !hasAccess) {
     return (
       <>
         <Item
